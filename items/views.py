b5a4bbc2186d76884578788dbab1eda08c3c1ca9
azusaMYK <11dctat@gmail.com>
2024-10-24 09:24:52 +0900

    show messages after create,update,delete items

--- items/views.py
+++ items/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -14,6 +15,14 @@ class ItemCreateView(CreateView):
   fields = ['name', 'price', 'memo']
   success_url = reverse_lazy('item:index')
 
+  def form_valid(self, form):
+    messages.success(self.request, 'Registration Completed')
+    return super().form_valid(form)
+
+  def form_invalid(self, form):
+    messages.error(self.request, 'Registration Failed')
+    return super().form_invalid(form)
+
 class ItemDetailView(DetailView):
   model = Item
 
@@ -24,7 +33,19 @@ class ItemUpdateView(UpdateView):
   def get_success_url(self):
     pk = self.kwargs.get('pk')
     return reverse('item:detail', kwargs={'pk': pk})
+  
+  def form_valid(self, form):
+    messages.success(self.request, 'Update Completed')
+    return super().form_valid(form)
+  
+  def form_invalid(self, form):
+    messages.error(self.request, 'Update Failed')
+    return super().form_invalid(form)
 
 class ItemDeleteView(DeleteView):
   model = Item
   success_url = reverse_lazy('item:index')
+
+  def form_valid(self, form):
+    messages.success(self.request, 'Deletion Completed') # メッセージを表示
+    return super().form_valid(form)
